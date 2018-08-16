c1d825c378818d6cef917f6d14c705529a7caca3
Marcelo Nenzinho <mcneill.marcelo@yahoo.com.br>
2018-08-16 14:26:19 -0300

    Create Doc Validade JS
    validade js

---
+++ JavaScript/doc-validade.js
@@ -0,0 +1,27 @@
+https://viacep.com.br/
+http://www.receita.fazenda.gov.br/aplicacoes/atcta/cpf/funcoes.js
+//Verifica se CPF é válido
+function TestaCPF(strCPF) {
+    var Soma;
+    var Resto;
+    Soma = 0;   
+    //strCPF  = RetiraCaracteresInvalidos(strCPF,11);
+    if (strCPF == "00000000000")
+	return false;
+    for (i=1; i<=9; i++)
+	Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (11 - i); 
+    Resto = (Soma * 10) % 11;
+    if ((Resto == 10) || (Resto == 11)) 
+	Resto = 0;
+    if (Resto != parseInt(strCPF.substring(9, 10)) )
+	return false;
+	Soma = 0;
+    for (i = 1; i <= 10; i++)
+       Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (12 - i);
+    Resto = (Soma * 10) % 11;
+    if ((Resto == 10) || (Resto == 11)) 
+	Resto = 0;
+    if (Resto != parseInt(strCPF.substring(10, 11) ) )
+        return false;
+    return true;
+}
